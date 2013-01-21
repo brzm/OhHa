9135e64027fd592db359d4209bf951e60a8c79ff
unknown <risto.karppinen@helsinki.fi>
2013-01-21 18:31:17 +0200

    jeejeejerry cotton

--- projekti/Sanapeli/src/sanapeli/Sanapeli.java
+++ projekti/Sanapeli/src/sanapeli/Sanapeli.java
@@ -1,10 +1,21 @@
-
 package sanapeli;
 
+import java.util.Scanner;
 
 public class Sanapeli {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here    
+
+        Sanat di = new Sanat("koira", "dog");
+        Sanat du = new Sanat("kissa", "cat");
+        Sanantutkiminen es = new Sanantutkiminen();
+        System.out.println(di);
+        System.out.println(du);
+        System.out.println(es);
+        
+        Scanner skanneri = new Scanner(System.in);
+        Kayttoliittyma seppo = new Kayttoliittyma(skanneri, es);
+        seppo.kaynnista();
     }
 }
